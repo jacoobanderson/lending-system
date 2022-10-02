1f53e76c4c980cf33c6e4dc091040ab6c34172dc
jacoobanderson <j00cwa@gmail.com>
2022-10-02 13:46:25 +0200

    Add structure and comments

--- app/src/main/java/model/Member.java
+++ app/src/main/java/model/Member.java
@@ -1,59 +1,105 @@
 package model;
 
+/**
+ * The type member.
+ */
 public class Member {
-    private String uniqueId;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phoneNumber;
-    private String createdAt;
+  // private String uniqueId;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String phoneNumber;
+  // private String createdAt;
 
-    public Member(String firstName, String lastName, String email, String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
+  /**
+   * Instantiates a new member.
+   *
+   * @param firstName   The first name.
+   * @param lastName    The last name.
+   * @param email       The email.
+   * @param phoneNumber The phone number.
+   */
+  public Member(String firstName, String lastName, String email, String phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+  }
 
-    public String getFirstName() {
-        return firstName;
-    }
+  /**
+   * Gets the first name.
+   *
+   * @return The first name.
+   */
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+  /**
+   * Sets the first name.
+   *
+   * @param firstName The first name.
+   */
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  /**
+   * Gets the last name.
+   *
+   * @return The last name.
+   */
+  public String getLastName() {
+    return lastName;
+  }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+  /**
+   * Sets the last name.
+   *
+   * @param lastName The last name.
+   */
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  /**
+   * Gets the email.
+   *
+   * @return The email.
+   */
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  /**
+   * Sets the email.
+   *
+   * @param email The email.
+   */
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+  /**
+   * Gets the phone number.
+   *
+   * @return The phone number.
+   */
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+  /**
+   * Sets the phone number.
+   *
+   * @param phoneNumber The phone number.
+   */
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
 
-    // public String generateId() {
+  // public String generateId() {
 
-    // }
-
-    public void setId() {
-
-    }
+  // }
 
 }
-
