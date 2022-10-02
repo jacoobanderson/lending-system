2a8460dc2595267f8bceaeef3cb9367d8453a124
jacoobanderson <j00cwa@gmail.com>
2022-10-02 15:47:46 +0200

    Create initial menu views

--- app/src/main/java/view/ConsoleUi.java
+++ app/src/main/java/view/ConsoleUi.java
@@ -1,5 +1,40 @@
 package view;
 
+import java.util.Scanner;
+
 public class ConsoleUi {
-    
+  private Scanner scan;
+
+  public ConsoleUi() {
+    this.scan = new Scanner(System.in, "utf-8");
+  }
+
+  public String getInput() {
+    return scan.nextLine();
+  }
+
+  public String printMainMenu() {
+    System.out.println("1. Create a member");
+    System.out.println("2. Select a specific member");
+    System.out.println("3. Show members");
+    return getInput();
+  }
+
+  public String printSpecificMemberMenu() {
+    System.out.println("1. Show information");
+    System.out.println("2. Change Information");
+    System.out.println("3. Create a new item");
+    System.out.println("4. Select an item");
+    System.out.println("5. Create new contract");
+    System.out.println("6. Delete this member");
+    return getInput();
+  }
+
+  public String printShowAllMembersMenu() {
+    System.out.println("1. Show in a simple way");
+    System.out.println("2. Show in a verbose way");
+    return getInput();
+  }
+
+
 }
