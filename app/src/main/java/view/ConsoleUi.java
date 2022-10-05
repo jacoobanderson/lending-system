64b8c4f2dd5393a976b6ceb900cb7f465bf164c2
jacoobanderson <j00cwa@gmail.com>
2022-10-05 12:49:07 +0200

    Create main menu input

--- app/src/main/java/view/ConsoleUi.java
+++ app/src/main/java/view/ConsoleUi.java
@@ -9,15 +9,16 @@ public class ConsoleUi {
     this.scan = new Scanner(System.in, "utf-8");
   }
 
-  public String printMainMenu() {
+  public MainMenuEvent printMainMenu() {
     System.out.println("1. Create a member");
     System.out.println("2. Select a specific member");
     System.out.println("3. Show in a simple way");
     System.out.println("4. Show in a verbose way");
     System.out.println("5. Advance 1 day");
+    return getMainMenuInput();
   }
 
-  public String printSpecificMemberMenu() {
+  public void printSpecificMemberMenu() {
     System.out.println("1. Show information");
     System.out.println("2. Change Information");
     System.out.println("3. Create a new item");
@@ -26,14 +27,30 @@ public class ConsoleUi {
     System.out.println("6. Delete this member");
   }
 
-  public 
+  public MainMenuEvent getMainMenuInput() {
+    switch(scan.nextInt()) {
+      case 1:
+        return MainMenuEvent.CREATE_MEMBER;
+      case 2:
+        return MainMenuEvent.SELECT_SPECIFIC_MEMBER;
+      case 3:
+        return MainMenuEvent.SHOW_SIMPLE_WAY;
+      case 4:
+        return MainMenuEvent.SHOW_VERBOSE_WAY;
+      case 5:
+        return MainMenuEvent.ADVANCE_ONE_DAY;
+      default:
+        return MainMenuEvent.QUIT;
+    }
+  }
 
   public enum MainMenuEvent {
     CREATE_MEMBER,
     SELECT_SPECIFIC_MEMBER,
     SHOW_SIMPLE_WAY,
     SHOW_VERBOSE_WAY,
-    ADVANCE_ONE_DAY
+    ADVANCE_ONE_DAY,
+    QUIT
   }
 
   public enum SpecificMemberEvent {
@@ -42,7 +59,8 @@ public class ConsoleUi {
     CREATE_ITEM,
     SELECT_ITEM,
     CREATE_CONTRACT,
-    DELETE_MEMBER
+    DELETE_MEMBER,
+    BACK
   }
 
 }
